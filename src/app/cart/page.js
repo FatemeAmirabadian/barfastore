eaa8aed28b0ee9d1f66c5465ff4af214d7d2a1d5
Fateme Amirabadian <fff.amirabadian@gmail.com>
2025-10-13 12:54:09 +0330

    solve errors in build time

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,12 +1,11 @@
 import CartPage from "@/components/CartPage";
 import React from "react";
-import { getProducts } from "../../../lib/helpers";
 
 export default async function page() {
-  const cartProducts = await getProducts();
+
   return (
     <div>
-      <CartPage products={cartProducts} />
+      <CartPage/>
     </div>
   );
 }
--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,17 +1,21 @@
 import ProductCard from "./modules/ProductCard";
 import PublicLayout from "./layouts/PublicLayout";
+import { getProducts } from "../../lib/helpers";
 
 export default async function ProductsPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`);
-  const products = await res.json();
-  console.log(products);
+  let products = [];
+  try {
+    products = await getProducts(); // fetch روی سرور runtime انجام میشه
+  } catch (err) {
+    console.error("Failed to fetch products:", err);
+  }
   return (
     <PublicLayout>
       <div className="py-8 px-4 max-w-6xl mx-auto">
-        <div className="flex flex-col-reverse flex-cols-2 sm:flex-cols-3 md:flex-cols-4 xl:flex-cols-5 gap-4">
-          {products.map((product) => {
-            return <ProductCard key={product.id} product={product} />;
-          })}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </PublicLayout>
