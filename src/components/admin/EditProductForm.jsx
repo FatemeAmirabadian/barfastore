a28dcaf02485af9572ac98b10b4f0fc57abf7b9e
Fateme Amirabadian <fff.amirabadian@gmail.com>
2025-10-13 18:12:35 +0330

    some changes

--- src/components/admin/EditProductForm.jsx
+++ src/components/admin/EditProductForm.jsx
@@ -1,10 +1,14 @@
+import { getProducts } from "../../../lib/helpers";
 import ProductForm from "./ProductForm";
 
 export default async function EditProductForm({ params }) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`);
-  const products = await res.json();
-  const product = products.find(p=> p.slug===params.slug)
+  let products = [];
+  try {
+    products = await getProducts();
+  } catch (err) {
+    console.error("Failed to fetch products:", err);
+  }
+  const product = products.find((p) => p.slug === params.slug);
 
-  return  <ProductForm mode="edit" initialData={product} />;
-
+  return <ProductForm mode="edit" initialData={product} />;
 }
